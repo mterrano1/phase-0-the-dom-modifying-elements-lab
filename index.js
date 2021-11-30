ff291d36aa3e38cc9f71b6768ac355aeb8fcb45a
Christopher Terrano <mterrano1@yahoo.com>
2021-11-30 17:42:29 -0600

    commit tree 1b0a9c17e0fc60ab51a6c738aeb0bd0ddc7c045a

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 // Write your code here!
-document.querySelector('main').remove();
+document.querySelector("main").remove();
 
 const newHeader = document.createElement("h1");
 
 newHeader.id = "victory";
 
-newHeader.innerHTML = "Mike is the champion";
-
-const newHeaderId = document.getElementById("victory");
-
-
-
-
-
-
-
+newHeader.textContent = "Mike is the champion";
 
 
